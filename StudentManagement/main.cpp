7b1a889604246fc71e0545bd029016a38abe7d4f
大为 罗 <2742394364@qq.com>
2026-08-11 23:01:13 +0800

    该版本添加的功能:添加学生功能

--- StudentManagement/main.cpp
+++ StudentManagement/main.cpp
@@ -1,17 +1,53 @@
 #include<iostream>
 #include<string>
+#include<vector>
 using namespace std;
+
 class Student {
 private:
+	long long m_number;
 	string m_name;
 	int m_age;
-	int m_number;
 
 public:
-
+	Student( long long number,string name, int age) {
+		m_number = number;
+		m_name = name;
+		m_age = age;
+	}
+	string GetName()const {
+		return m_name;
+	}
+	int GetAge() const{
+		return m_age;
+	}
+	long long GetNumber()const {
+		return m_number;
+	}
 };
+bool addStudent(vector<Student>& students) {
+	string name;
+	int age;
+	long long number;
+	cout << "请输入学生学号" << endl;
+	cin >> number;
+	for (int i = 0; i < students.size(); i++) {
+		if (students[i].GetNumber() == number) {
+			return false;
+		}
+	}
+	cout << "姓名" << endl;
+	cin >> name;
+	cout<< "年龄" << endl;
+	cin >> age;
+	Student s(number, name, age);
+	students.push_back(s);
+	return true;
+}
+
 int main() {
 	int choice;
+	vector<Student> students;
 	while (true) {
 		cout << "1添加学生" << endl;
 		cout << "2查看所有学生" << endl;
@@ -23,7 +59,12 @@ int main() {
 		case 0:
 			return 0;
 		case 1:
-			cout << "该项目开发中" << endl;
+			if (addStudent(students)) {
+				cout << "添加成功" << endl;
+			}
+			else {
+				cout << "该学生已存在，请重新输入" << endl;
+			}
 			break;
 		case 2:
 			cout << "该项目开发中" << endl;
